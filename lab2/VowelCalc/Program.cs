67d39706131f756394e0f4f3fd1caaf2d78ce5b8
alisa-teplouhova <flovettee@gmail.com>
2017-03-04 10:48:21 +0300

    VowelCalc первый коммит

---
+++ lab2/VowelCalc/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.Owin.Hosting;
+using System.Configuration;
+using System.Net.Http;
+
+namespace VowelCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string baseAddress = ConfigurationManager.AppSettings["baseAddress"];
+
+            // Start OWIN host 
+            using (WebApp.Start<Startup>(url: baseAddress))
+            {
+                Console.WriteLine("Vowel calc is working... ");
+                Console.WriteLine("Press Enter to exit.");
+
+                Console.ReadLine(); 
+            }
+        }
+    }
+}
---
+++ lab2/VowelCalc/ValuesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Http;
+using System.Threading;
+
+namespace VowelCalc
+{
+    public class ValuesController : ApiController 
+    {
+        public ValuesController()
+        {
+        }
+
+        // POST api/values/5
+        public IHttpActionResult Post(string id, [FromBody]string value)
+        {
+            Thread.Sleep(1000);
+            return StatusCode(System.Net.HttpStatusCode.OK);
+        }
+    }
+}
