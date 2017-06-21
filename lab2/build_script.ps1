00063660ba6479c1562ff6992ed1e1b56cb92dbf
alisa-teplouhova <flovettee@gmail.com>
2017-06-22 01:03:29 +0300

    Lab 8. Powershell script. Поправила форматирование.

--- lab2/build_script.ps1
+++ lab2/build_script.ps1
@@ -7,17 +7,17 @@ $prodDir = "$projDir\Release"
 #check directories
 if (-Not (Test-Path $msbuild)) {
     Write-Error "Error: MSBUILD not found"
-	exit
+    exit
 }
 
 if (-Not (Test-Path $solution)) {
     Write-Error "Error: Solution not found"
-	exit
+    exit
 }
 
 #clear temp folder
 If (Test-Path $tempDir){
-	Remove-Item $tempDir/*
+    Remove-Item $tempDir/*
 }
 
 #build solution
@@ -26,20 +26,20 @@ $collectionOfArgs = @("$solution", "/p:OutputPath=$tempDir", "/p:Configuration=R
 
 #close current applications
 If (Test-Path $prodDir){
-	$files = Get-ChildItem -path $prodDir -Recurse -Include *.exe
-	foreach ($file in $files)
-	{
-		$process = Get-Process $file.BaseName -ErrorAction SilentlyContinue
-		if ($process) {
-			$process.CloseMainWindow()
-			if (!$process.HasExited) {
-				$process | Stop-Process -Force -ErrorAction Stop
-			}
-		}
-	}
-	Sleep 1
-	Remove-Item $prodDir/* -Recurse
-	Remove-Item $prodDir
+    $files = Get-ChildItem -path $prodDir -Recurse -Include *.exe
+    foreach ($file in $files)
+    {
+        $process = Get-Process $file.BaseName -ErrorAction SilentlyContinue
+        if ($process) {
+            $process.CloseMainWindow()
+            if (!$process.HasExited) {
+                $process | Stop-Process -Force -ErrorAction Stop
+            }
+        }
+    }
+    Sleep 1
+    Remove-Item $prodDir/* -Recurse
+    Remove-Item $prodDir
 }
 
 #rename tmp folder
